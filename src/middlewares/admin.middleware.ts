e28281e21ad297b32e63ee29220bcb8c8f32ce9b
rikwilisbr <henriquewilliamstos@gmail.com>
2024-07-02 21:24:19 -0300

    refactor: change the admin pages to local

--- src/middlewares/admin.middleware.ts
+++
@@ -1,6 +0,0 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-
-export default async function AdminMiddleware(request: NextRequest) {
-    return NextResponse.redirect(new URL('/admin/dashboard', request.url))
-  }
